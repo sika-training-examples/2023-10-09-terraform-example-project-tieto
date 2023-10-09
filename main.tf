9f5e9ce457ddbf25c61bd9cec3b1c98ab9b30467
Ondrej Sika <ondrej@ondrejsika.com>
2023-10-09 09:04:51 +0000

    feat: Add data source, random & output example

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.75.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
   }
 }
 
@@ -21,7 +25,45 @@ provider "azurerm" {
   subscription_id = var.azurerm_subscription_id
 }
 
+provider "random" {}
+
+resource "random_string" "suffix" {
+  length  = 10
+  lower   = true
+  upper   = false
+  numeric = true
+  special = false
+}
+
 resource "azurerm_resource_group" "main" {
-  name     = "tt-ondrej"
+  name     = "mycorp-eu-project-team-ondrej-${random_string.suffix.result}"
   location = "westeurope"
+}
+
+data "azurerm_resource_group" "global" {
+  name = "tt-global"
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "example${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_account" "global" {
+  name                     = "exampleglobal${random_string.suffix.result}"
+  resource_group_name      = data.azurerm_resource_group.global.name
+  location                 = data.azurerm_resource_group.global.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+output "storage_account_main_name" {
+  value = azurerm_storage_account.main.name
+}
+
+output "storage_account_globa_name" {
+  value = azurerm_storage_account.global.name
 }
